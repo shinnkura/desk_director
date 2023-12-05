36d55afa4853432bfb2f8f67d28e417837ed029d
shinnkura <n.shinzo1010@icloud.com>
2023-12-05 11:21:43 +0900

    perf(lib): ディレクトリ構成の修正

--- lib/screens/dashboard/components/data_widget.dart
+++ lib/screens/dashboard/components/data_widget.dart
@@ -1,6 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:dashboard/dashboard.dart';
-import 'storage.dart';
+import '../../../services/storage.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher_string.dart';
